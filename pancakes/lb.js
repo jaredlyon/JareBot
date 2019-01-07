6ea52d93ea051b1c4f812d499142e109c745f645
KingWaffleshnoz <jaredalyon@gmail.com>
2019-01-07 01:41:22 -0800

    Leaderboard redundancy
    A redundant/abbreviated command for 'j!leaderboard'

---
+++ pancakes/lb.js
@@ -0,0 +1,31 @@
+module.exports = {
+  name: 'lb',
+  permission: 1,
+  main: function (bot, msg) {
+    var Discord = require('discord.js'), bank = [];
+    for (var val in bot.bank) {
+      bot.bank[val].userID = val;
+      bank.push(bot.bank[val])
+    }
+    bank.sort((a, b) => parseFloat(b.items.pancakes) - parseFloat(a.items.pancakes));
+
+    var lb = new Discord.RichEmbed()
+      .setColor(msg.guild.me.displayHexColor)
+      .setTitle('🥞 Pancakes Leaderboard 🥞')
+      .setFooter("© JL's Diner 2019", msg.guild.iconURL);
+
+    for (var i = 0; i < 12; i++) {
+    var bankee = bot.users.get(bank[i].userID)
+
+      if (bankee == null) {
+        bankee = 'User Left Server'
+      } else {
+        bankee = bot.users.get(bank[i].userID).username
+      }
+
+      lb.addField(`${i + 1}: ${bankee}`, bank[i].items.pancakes, true);
+    }
+
+    msg.channel.send({ embed: lb });
+  }
+};
