ebc0ed636949162a63062c744dde99f33c5e3a0f
Jared Lyon <jaredalyon@gmail.com>
2019-10-23 16:40:56 -0700

    6.0.0
    info update

--- help/info.js
+++ help/info.js
@@ -19,12 +19,12 @@ module.exports = {
                 value: "JareBot is Discord chat bot (obviously) that performs rudimentary chat tasks as well as executes complicated score and currency commands. This bot is confined to Jared's personal Discord server, and is primarily designed to assist in giveaways, moderation, and a wide array of information distribution. JareBot was initially created in August of 2017 by Michael Cao in order to teach its current developer, Jared Lyon, how to code in Javascript."
               },
               {
-                name: "Last Update: v5.3.6",
-                value: "-Added fishing; includes three new commands and an extra storage file\n-Added passive income and fishing to currency statistics\n-Updated j!pancake, j!balance, and j!statistics modules so that users can check other user data\n-Updated j!sleep\n-Rewrote majority of the help blurbs and added the fishing section\n-Fixed issue where blackjack would keep the bet after a session timeout"
+                name: "Last Update: v6.0.0",
+                value: "-Migrated bot to RethinkDB\n-Rebalanced fishing module\n-General cleanup and bug fixes"
               },
               /*CHECK GITHUB FOOTERS*/
               {
-                name: "GitHub: v5.3.6",
+                name: "GitHub: v6.0.0",
                 value: "https://github.com/KingWaffleshnoz/JareBot\nNote that this page is still a WIP."
               }
             ]
