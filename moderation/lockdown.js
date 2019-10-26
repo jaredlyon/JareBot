2debed4091b917657718703f5790132d80baca3e
Jared Lyon <jaredalyon@gmail.com>
2019-10-25 17:45:51 -0700

    moderation patch

--- moderation/lockdown.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'lockdown',
-    permission: 2,
-    main: function(bot, msg) {
-        msg.guild.members.forEach(member => {
-            member.setRoles(["399740949707882497"], "Lockdown disengaged!")
-        });
-    }
-};
