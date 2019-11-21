3971f177407553a777a3990b70da4735e61cb99e
Jared Lyon <jaredalyon@gmail.com>
2019-11-20 19:26:22 -0800

    fix coinflip & info update

--- help/info.js
+++ help/info.js
@@ -1,34 +1,34 @@
 module.exports = {
-    name: 'info',
-    permission: 1,
-    main: function (bot, msg) {
-        msg.channel.send({
-          embed: 
-          { color: 3447003,
-            footer: {
-              icon_url: msg.guild.iconURL,
-              text: msg.guild.name 
+  name: 'info',
+  permission: 1,
+  main: function (bot, msg) {
+      msg.channel.send({
+        embed: 
+        { color: 3447003,
+          footer: {
+            icon_url: msg.guild.iconURL,
+            text: msg.guild.name 
+          },
+          author: {
+            name: "Bot Information",
+            icon_url: msg.guild.iconURL
+          },
+          fields: [
+            {
+              name: "General:",
+              value: "JareBot is Discord chat bot (obviously) that performs rudimentary chat tasks as well as executes complicated score and currency commands. This bot is confined to Jared's personal Discord server, and is primarily designed to assist in giveaways, moderation, and a wide array of information distribution. JareBot was initially created in August of 2017 by Michael Cao in order to teach its current developer, Jared Lyon, how to code in Javascript."
             },
-            author: {
-              name: "Bot Information",
-              icon_url: msg.guild.iconURL
+            {
+              name: "Last Update: v6.1.0",
+              value: "-Added a votekick function\n-Fixed coinflip bug"
             },
-            fields: [
-              {
-                name: "General:",
-                value: "JareBot is Discord chat bot (obviously) that performs rudimentary chat tasks as well as executes complicated score and currency commands. This bot is confined to Jared's personal Discord server, and is primarily designed to assist in giveaways, moderation, and a wide array of information distribution. JareBot was initially created in August of 2017 by Michael Cao in order to teach its current developer, Jared Lyon, how to code in Javascript."
-              },
-              {
-                name: "Last Update: v6.0.1",
-                value: "-Cleaned up several hundred lines of code\n-Minor bug fixes\n-Revamped footer and ID system"
-              },
-              /*CHECK GITHUB FOOTERS*/
-              {
-                name: "GitHub: v6.0.1",
-                value: "https://github.com/KingWaffleshnoz/JareBot\nNote that this page is still a WIP."
-              }
-            ]
-          }
-        });
-    }
+            /*CHECK GITHUB FOOTERS*/
+            {
+              name: "GitHub: v6.1.0",
+              value: "https://github.com/KingWaffleshnoz/JareBot\nNote that this page is still a WIP."
+            }
+          ]
+        }
+      });
+  }
 }
