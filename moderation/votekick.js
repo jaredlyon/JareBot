c709598cca0a0774f85388c85d2634af19f46cd3
Jared Lyon <jaredalyon@gmail.com>
2019-11-26 01:33:46 -0800

    Update votekick.js

--- moderation/votekick.js
+++ moderation/votekick.js
@@ -1,46 +1,124 @@
 module.exports = {
     name: 'votekick',
-    permission: 1,
-    main: function (bot, msg) {
-        /** Need to add
-         * Calculate votes needed
-         * majority system
-         * remove from voice
-         * figure out what channels to embed into
-         */
+    permission: 2,
+    main: async function (bot, msg) {
+        if (msg.mentions.users.first()) {
+            //grab target
+            var target = msg.mentions.users.first();
 
+            //grab and insert reason
+            var reason = msg.content.split(' ').splice(1).join(' ');
+            if (reason === '') {
+                reason = '(no reason given)'
+            };
 
-        var channel = ;
-        var log = ;
-        var target = msg.mentions.users.array()[0];
-        var user = bot.users.get(target.id);
-        var reason = msg.content.split(' ').splice(1).join(' ');
-        if (reason === '') {
-            reason = 'No reason was specified.'
-        };
-        var kick = new Discord.RichEmbed()
-            .setAuthor(user.username, user.avatarURL)
-            .addField('Member kicked:', ``)
-            .addField('Reason:', reason)
-            .setFooter(bot.user.username, bot.user.avatarURL)
-            .setTimestamp()
-            .setColor(3447003);
+            //count votes
+            var yes = 0;
+            var no = 0;
+            let vcCount = //need some way to count members
+            console.log(vcCount);
+            var threshold = Math.floor(vcCount/2);
+            console.log(threshold);
 
-        if (msg.mentions.members) {
-            msg.mentions.members.forEach(member => {
-                member./*kick from voice*/.then(member => {
-                    msg.reply(member + " has been banned!")
-
-                    channel.send({
-                        embed: kick
-                    })
-                    log.send({
-                        embed: kick
-                    })
+            if (target.voiceChannel) {
+                let vote = await msg.channel.send({
+                    embed: {
+                        color: 3447003,
+                        title: msg.author + " wants to call a vote:",
+                        description:
+                            "Kick player: " +
+                            target.username +
+                            "?\n؜" +
+                            "React 👍 to vote YES" +
+                            "\n" + 
+                            "React 👎 to vote NO",
+                        footer: {
+                            text: "Vote count:",
+                        },
+                        timestamp: new Date()
+                    }
+                })
+                .then(async function (message) {
+                    await message.react("👍")
+                    await message.react("👎")
+                    return message;
                 });
-            })
+
+                let filter = (reaction, user) =>
+                (reaction.emoji.name === "👍" || reaction.emoji.name === "👎");
+
+                //play table
+                let collector = vote.createReactionCollector(filter, {
+                    time: 1000 * 2 * 60
+                });
+
+                collector.on("collect", async messageReaction => {
+                    if (messageReaction.emoji.name === "👍") {
+                        yes++;
+                    } else if (messageReaction.emoji.name === "👎") {
+                        no++;
+                    }
+                });
+
+                collector.on("end", async collected => {
+                    if (timeout) {
+                        if (yes >= threshold || yes > no) {
+                            vote.edit({
+                                embed: {
+                                    color: 3447003,
+                                    title: msg.author + " wants to call a vote:",
+                                    description:
+                                        "Kick player: " +
+                                        target.username +
+                                        "?\n؜" +
+                                        "**Vote passed!**",
+                                    footer: {
+                                        text: "Vote count:",
+                                    },
+                                    timestamp: new Date()
+                                }
+                            });
+                            target.setVoiceChannel(null)
+                        } else if (yes <= threshold || yes <= no) {
+                            vote.edit({
+                                embed: {
+                                    color: 3447003,
+                                    title: msg.author + " wants to call a vote:",
+                                    description:
+                                        "Kick player: " +
+                                        target.username +
+                                        "?\n؜" +
+                                        "**Vote failed!**",
+                                    footer: {
+                                        text: "Vote count:",
+                                    },
+                                    timestamp: new Date()
+                                }
+                            });
+                        } else {
+                            vote.edit({
+                                embed: {
+                                    color: 3447003,
+                                    title: msg.author + " wants to call a vote:",
+                                    description:
+                                        "Kick player: " +
+                                        target.username +
+                                        "?\n؜" +
+                                        "**Vote failed!**",
+                                    footer: {
+                                        text: "Vote count:",
+                                    },
+                                    timestamp: new Date()
+                                }
+                            });
+                        }
+                    }
+                });
+            } else {
+                msg.reply("cannot kick a disconnected user!");
+            }
         } else {
-            msg.reply("mention someone!")
+            msg.reply("mention someone!");
         }
     }
 };
