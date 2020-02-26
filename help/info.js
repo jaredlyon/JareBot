e2d4587a6a8a875552fa939a961e8dd8c69ac8b1
Jared Lyon <jaredalyon@gmail.com>
2020-02-26 14:01:00 -0800

    reduce fishing cooldown

--- help/info.js
+++ help/info.js
@@ -20,7 +20,7 @@ module.exports = {
             },
             {
               name: "Last Update: v7.1.0",
-              value: "-Added a votekick function\n-Rebalanced income portion of the economy\n-Touched up portions of the UI\n-Updated README"
+              value: "-Added a votekick function\n-Rebalanced income portion of the economy\n-Touched up portions of the UI\n-Updated README\n-Lowered fishing cooldown from 7 to 5 seconds"
             },
             /*CHECK GITHUB FOOTERS*/
             {
