cdf49d5cab2a69f5896e73f505ab806838b5413c
Jared Lyon <jaredalyon@gmail.com>
2020-03-14 18:34:12 -0700

    fix status

--- events/ready.js
+++ events/ready.js
@@ -3,8 +3,8 @@ exports.run = async (bot, msg) => {
 	bot.awaitConsoleInput();
 	await bot.startDatabase();
 
-	bot.user.setPresence({ status: 'online', game: { name: 'Lego Legions | j!help', type: 0 } });
-	//bot.user.setPresence({ status: 'online', game: { name: "It's not delivery, it's DiBjorno", type: 0 } });
+	bot.user.setPresence({ status: 'online', activity: { name: 'Lego Legions | j!help', type: 0 } });
+	//bot.user.setPresence({ status: 'online', activity: { name: "It's not delivery, it's DiBjorno", type: 0 } });
 
 	bot.log(`${bot.user.username} is online and ready to serve in ${bot.channels.size} channels on ${bot.guilds.size} servers!`);
 }
