1d76d6b0a88323fb203935763fe3fb89ee779cf4
Michael Cao <mike.x.cao@gmail.com>
2020-03-21 10:39:07 -0400

    fixed purge

--- moderation/purge.js
+++ moderation/purge.js
@@ -1,12 +1,12 @@
 module.exports = {
 	name: 'purge',
 	permission: 2,
-	main: function(bot, msg) {
-		const yup = bot.emojis.find("name", "yup");
+	main: function (bot, msg) {
+		const yup = bot.emojis.cache.find(emoji => emoji.name == "yup").toString();
 		if (msg.member.hasPermission('MANAGE_MESSAGES') || msg.author.id === require("../config.json").owner) {
 			var num = msg.content;
 			if (!isNaN(num)) {
-				msg.channel.fetchMessages({limit: num})
+				msg.channel.messages.fetch({ limit: num })
 					.then(messages => msg.channel.bulkDelete(messages))
 					.catch(msg.channel.bulkDelete);
 
