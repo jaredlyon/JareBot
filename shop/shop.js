8d0806c54df8b3a26ddc8edd99b9ff5f6b873291
Jared Lyon <jaredalyon@gmail.com>
2020-04-26 01:52:01 -0700

    add shop

--- shop/shop.js
+++ shop/shop.js
@@ -1,20 +1,31 @@
 module.exports = {
     name: "shop",
-    permission: 2,
-    main: function(bot, msg) {
+    permission: 1,
+    main: async function(bot, msg) {
         var userSelection = msg.content.split(' ').splice(0)[0]; //buy?
         var userProduct = msg.content.split(' ').splice(1)[0]; //which item?
+        let account = await bot.bank.get(msg.author.id);
+        let stats = await bot.stats.get(msg.author.id);
+        var channel = bot.channels.cache.get(bot.config.logChannel);
 
         if (userSelection == "buy") {
             if (userProduct == 1) {
-                //buy 1 month
+                account.items.pancakes -= 1000;
+                stats.pancakes.given += amt;
+                await bot.bank.update(account);
+                await bot.stats.update(stats);
+                msg.reply("purchase successful!");
+                channel.send(msg.author.id + " has purchased one month of nitro!");
             } else if (userProduct == 2) {
-                //buy 1 year
+                msg.reply("this item is currently unavailable! Sorry...")
+            } else {
+                msg.reply("please indicate which item you would like to purchase!\n Usage: `j!shop buy [1]`")
             }
         } else if (userSelection == null) {
             var menu = new Discord.MessageEmbed()
                 .setAuthor("Shop Inventory", msg.guild.iconURL())
-                .addField('One Month of Discord Nitro Classic', `1 item(s) available!`)
+                .addField('1. One Month of Discord Nitro Classic', `1000 pancakes`)
+                .addField('2. One Year of Discord Nitro Classic', `Unavailable`)
                 .setFooter(bot.user.username, bot.user.avatarURL())
             
             channel.send({embed: menu});
