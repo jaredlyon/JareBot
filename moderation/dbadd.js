ad60f3ef3c2940f9cbf8b45ee303e7a313653be5
Jared Lyon <jaredalyon@gmail.com>
2020-04-26 03:31:43 -0700

    final dbadd fix

--- moderation/dbadd.js
+++ moderation/dbadd.js
@@ -4,22 +4,25 @@ module.exports = {
     main: async function(bot, msg) {
         var target = msg.mentions.users.array()[0];
         var selection = msg.content.split(' ').splice(1)[0];
-        var amount = msg.content.split(' ').splice(2)[0];
+        var amount = Number(msg.content.split(' ').splice(2)[0]);
 
         if (target != null) {
             let account = await bot.bank.get(target.id);
-            let stats = await bot.stats.get(target.id);
             var channel = bot.channels.cache.get(bot.config.logChannel);
             if (selection == "money") {
                 if (amount != null) {
                     account.balance += amount;
-                    channel.send(msg.author.id + " added $" + amount + " to " + target.toString() + "'s account.");
+                    await bot.bank.update(account);
+                    channel.send(msg.author.toString() + " added $" + amount + " to " + target.toString() + "'s account.");
+                    msg.reply("success! Log generated.")
                 } else {
                     msg.reply("input amount, dummy!");
                 }
             } else if (selection == "pancakes") {
                 account.items.pancakes += amount;
-                channel.send(msg.author.id + " added " + amount + " stack(s) of pancakes to " + target.toString() + "'s account.");
+                await bot.bank.update(account);
+                channel.send(msg.author.toString() + " added " + amount + " stack(s) of pancakes to " + target.toString() + "'s account.");
+                msg.reply("success! Log generated.")
             } else {
                 msg.reply("input amount, dummy!");
             }
