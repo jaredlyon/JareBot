e18ef9b158d7c7e3351d5f7abd7a6521b0b1a662
Jared Lyon <jaredalyon@gmail.com>
2020-04-26 02:23:06 -0700

    fix shop

--- shop/shop.js
+++ shop/shop.js
@@ -1,3 +1,5 @@
+var Discord = require('discord.js');
+
 module.exports = {
     name: "shop",
     permission: 1,
@@ -6,31 +8,32 @@ module.exports = {
         var userProduct = msg.content.split(' ').splice(1)[0]; //which item?
         let account = await bot.bank.get(msg.author.id);
         let stats = await bot.stats.get(msg.author.id);
-        var channel = bot.channels.cache.get(bot.config.logChannel);
 
         if (userSelection == "buy") {
+            var channel = bot.channels.cache.get(bot.config.logChannel);
             if (userProduct == 1) {
-                account.items.pancakes -= 1000;
-                stats.pancakes.given += amt;
-                await bot.bank.update(account);
-                await bot.stats.update(stats);
-                msg.reply("purchase successful!");
-                channel.send(msg.author.id + " has purchased one month of nitro!");
+                if (account.items.pancakes < 1000) {
+                    msg.reply("you do not have enough pancakes!");
+                } else if (account.items.pancakes >= 1000) {
+                    account.items.pancakes -= 1000;
+                    await bot.bank.update(account);
+                    await bot.stats.update(stats);
+                    msg.reply("purchase successful!");
+                    channel.send(msg.author.id + " has purchased one month of nitro!");
+                }
             } else if (userProduct == 2) {
                 msg.reply("this item is currently unavailable! Sorry...")
             } else {
-                msg.reply("please indicate which item you would like to purchase!\n Usage: `j!shop buy [1]`")
+                msg.reply("please properly indicate which item you would like to purchase!\n Usage: `j!shop buy [input]`")
             }
-        } else if (userSelection == null) {
+        } else {
             var menu = new Discord.MessageEmbed()
                 .setAuthor("Shop Inventory", msg.guild.iconURL())
                 .addField('1. One Month of Discord Nitro Classic', `1000 pancakes`)
                 .addField('2. One Year of Discord Nitro Classic', `Unavailable`)
                 .setFooter(bot.user.username, bot.user.avatarURL())
             
-            channel.send({embed: menu});
-        } else {
-            msg.reply("something went wrong!");
+            msg.channel.send({embed: menu});
         }
     }
 }
