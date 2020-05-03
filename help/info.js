33cd230992c9e63bce875b875a02aa4ceb3541f3
Jared Lyon <jaredalyon@gmail.com>
2020-05-03 04:51:27 -0700

    Update info.js

--- help/info.js
+++ help/info.js
@@ -20,14 +20,10 @@ module.exports = {
             value: "JareBot is Discord chat bot (obviously) that performs rudimentary chat tasks as well as executes complicated score and currency commands. This bot is confined to Jared's personal Discord server, and is primarily designed to assist in giveaways, moderation, and a wide array of information distribution. JareBot was initially created in August of 2017 by Michael Cao in order to teach its current developer, Jared Lyon, how to code in Javascript."
           },
           {
-            name: "Last Update: v8.1.0",
-            value: `- Added shop
-                    - Added dbadd command
-                    - Updated help
-                    - Updated rules
-                    - Updated blackjack dealer behavior
-                    - Updated the pancakes section in statistics
-                    - Rebalanced fishing economy`
+            name: "Last Update: v8.1.1",
+            value: `- Rebalanced fishing economy (again)
+                    - Improved UI elements
+                    - Improved code legibility and stability`
           },
           /*CHECK GITHUB FOOTERS*/
           {
