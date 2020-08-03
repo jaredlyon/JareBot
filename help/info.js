f4f84aa9a8aec18d15ee74d7037e31da6ce6fc6f
Jared Lyon <jaredalyon@gmail.com>
2020-08-02 20:32:05 -0700

    Update info.js

--- help/info.js
+++ help/info.js
@@ -20,22 +20,18 @@ module.exports = {
             value: "JareBot is a Discord chat bot that performs various chat functions as well as uses an array of RethinkDB tables to create an economy. This project was initially created in August of 2017 by Michael Cao in order to teach its current developer, Jared Lyon, how to code in Javascript."
           },
           {
-            name: "Last Update: v8.1.0",
-            value: `- Added shop
-                    - Added dbadd command
-                    - Updated help
-                    - Updated rules
-                    - Updated blackjack dealer behavior
-                    - Updated the pancakes section in statistics
-                    - Rebalanced fishing economy`
+            name: "Last Update: v8.2.0",
+            value: `- Rebalanced fishing economy (again)
+                    - Improved UI elements
+                    - Improved code legibility and stability
+                    - Added 'Is Jared at work?'
+                    - Added movie role
+                    - Added approval system
+                    - Added command aliases`
           },
           /*CHECK GITHUB FOOTERS*/
           {
-<<<<<<< Updated upstream
-            name: "GitHub: v8.0.0",
-=======
             name: "GitHub",
->>>>>>> Stashed changes
             value: "https://github.com/KingWaffleshnoz/JareBot\nNote that this page is still a WIP."
           }
         ]
