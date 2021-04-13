ceb17e13538074d7376815ee9b885b57d25a57a8
Jared Lyon <jaredalyon@gmail.com>
2021-04-12 21:33:11 -0400

    allow dbadd to add streaks

--- moderation/dbadd.js
+++ moderation/dbadd.js
@@ -8,6 +8,7 @@ module.exports = {
 
         if (target != null) {
             let account = await bot.bank.get(target.id);
+            let streaks = await bot.streaks.get(target.id);
             var channel = bot.channels.cache.get(bot.config.logChannel);
             if (selection == "money") {
                 if (amount != null) {
@@ -22,6 +23,11 @@ module.exports = {
                 account.items.pancakes += amount;
                 await bot.bank.update(account);
                 channel.send(msg.author.toString() + " added " + amount + " stack(s) of pancakes to " + target.toString() + "'s account.");
+                msg.reply("success! Log generated.")
+            } else if (selection == "streaks") {
+                streaks.streak += amount;
+                await bot.streaks.update(streaks);
+                channel.send(msg.author.toString() + " added " + amount + " days to " + target.toString() + "'s streak.");
                 msg.reply("success! Log generated.")
             } else {
                 msg.reply("input amount, dummy!");
