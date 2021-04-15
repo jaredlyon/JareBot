015d5fc2421cf8a026effc51cb6b8c012700aa5d
Jared Lyon <jaredalyon@gmail.com>
2021-04-14 23:21:27 -0400

    updates rules

--- help/rules.js
+++ help/rules.js
@@ -14,18 +14,19 @@ module.exports = {
         fields: [
           {
             name: "1. Be respectful.",
-            value: "This rule is fairly self-explainable, and also incredibly easy to apply. Generally, just try to maintain a positive attitude, respect boundaries, and don't be an asshole. In that same regard, speech considered hateful or otherwise unhealthy for the server's atmosphere is heavily subjective; consequently, speech that you find appropriate may not always be appreciated by your peers. With this in mind, please be conscious of how your words might affect those around you."
+            value: "Maintain a positive attitude, respect boundaries, and don't be overly combative. In that same regard, speech considered hateful or otherwise unhealthy for the server's atmosphere is heavily subjective; consequently, speech that you find appropriate may not always be appreciated by your peers. With this in mind, please be conscious of how your words might affect those around you."
           },
           {
-            name: "2. Use the appropriate text channels.",
-            value: "Since this server is primarily self-governed by those who are on it, I'm not going to always be around to enforce common sense. Don't post anything nsfw, stick the appropriate content in their respective channels, confine bot usage to #bot-console, and use the designated text channels when participating in voice calls."
+            name: "2. Follow all text and voice channel guidelines.",
+            value: "When utilizing text channels, please use the appropriate text channels in accordance with their respective topics, confine all bot commands to #bot-console, and do not post any nsfw content. Spam is not tolerated, including excessive advertising as well as the usage of distorted or needlessly loud noise in voice channels."
           },
           {
             name: "3. Do not spread disinformation.",
-            value: "The deliberate dissemination of disinformation can result in immediate removal from the community. Under this rule, dissemination of disinformation includes, but is not limited to, any conduct with intent to convey false or misleading information as fact under circumstances where such information may reasonably be believed."
+            value: "The deliberate dissemination of disinformation will result in immediate removal from the server. Specifically, dissemination of disinformation includes, but is not limited to, any conduct with intent to convey false or misleading information as fact under circumstances where such information may reasonably be believed."
           }
         ]
       }
     });
+    msg.channel.send("https://discord.gg/q2fCsNr");
   }
 }
