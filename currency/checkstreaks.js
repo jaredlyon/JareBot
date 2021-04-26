8f9af34c84edc4a5dac80b01b6fd795c849499ca
Jared Lyon <jaredalyon@gmail.com>
2021-04-26 13:44:39 -0700

    Create checkstreaks.js

---
+++ currency/checkstreaks.js
@@ -0,0 +1,34 @@
+module.exports = {
+    name: "checkstreaks",
+    permission: 1,
+    main: async function (bot, msg) {
+        var Discord = require('discord.js');
+        const activeStreaks = new Array();
+
+        //creates an array of all active streaks
+        await bot.guilds.cache.get("399740385221672970").members.cache.forEach(async member => {
+            let streaks = (await bot.streaks.get(member.id)) || {};
+
+            if (streaks.streak >= 1 && new Date() - new Date(streaks.lastDaily) <= 172800000) {
+                activeStreaks.push(member.id.displayName);
+                console.log("Added " + member.displayName + "'s streak to the array!");
+            }
+        });
+
+        //message header
+        var message = "**__Active Daily Streaks:__**\n*Does not include streaks that have expired.*\n";
+
+        //add all usernames from the active streak array to the message
+        var i;
+        var listLength;
+        listLength = activeStreaks.length;
+        console.log("List Length: " + listLength);
+        for (i = 0; i < activeStreaks.length; i++) {
+            message += activeStreaks[i];
+            message += "\n" + ",";
+        };
+
+        //sends the message
+        msg.channel.send(message);
+    }
+}
