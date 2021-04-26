6a73af5ce23e3b8cc9010c721c6340f416a45475
Jared Lyon <jaredalyon@gmail.com>
2021-04-26 14:01:11 -0700

    Update checkstreaks.js

--- currency/checkstreaks.js
+++ currency/checkstreaks.js
@@ -2,18 +2,19 @@ module.exports = {
     name: "checkstreaks",
     permission: 1,
     main: async function (bot, msg) {
-        var Discord = require('discord.js');
         const activeStreaks = new Array();
 
         //creates an array of all active streaks
-        await bot.guilds.cache.get("399740385221672970").members.cache.forEach(async member => {
+        bot.guilds.cache.get("399740385221672970").members.cache.forEach(async member => {
             let streaks = (await bot.streaks.get(member.id)) || {};
 
             if (streaks.streak >= 1 && new Date() - new Date(streaks.lastDaily) <= 172800000) {
-                activeStreaks.push(member.id.displayName);
+                var name = member.displayName;
+                activeStreaks.push(name);
                 console.log("Added " + member.displayName + "'s streak to the array!");
-            }
+            };
         });
+        console.log(activeStreaks);
 
         //message header
         var message = "**__Active Daily Streaks:__**\n*Does not include streaks that have expired.*\n";
