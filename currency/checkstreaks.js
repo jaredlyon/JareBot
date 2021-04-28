4a65b8aa757b66f595a071b5f43b071243276e02
Jared Lyon <jaredalyon@gmail.com>
2021-04-28 12:54:39 -0700

    Update checkstreaks.js

--- currency/checkstreaks.js
+++ currency/checkstreaks.js
@@ -3,15 +3,16 @@ module.exports = {
     permission: 1,
     main: function (bot, msg) {
         var message = "**__Active Daily Streaks:__**\n*Does not include streaks that have expired.*\n";
-
-        bot.guilds.cache.get("399740385221672970").members.cache.each(async member => {
+ 
+        let members = bot.guilds.cache.get("399740385221672970").members.cache;
+        for (let member of members) {
             bot.streaks.get(member.id).then(streaks => {
                 if (streaks.streak && streaks.streak >= 1 && (new Date() - new Date(streaks.lastDaily) <= 172800000)) {
                     message += member.displayName + "\n" + ",";
                     console.log("Added " + member.displayName + "'s streak to the array!");
-                };
+                }
             });
-        });
+        };
 
         //sends the message
         msg.channel.send(message);
