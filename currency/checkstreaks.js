a279a7925c032ec667c99eb7d32bf18fd1721761
Jared Lyon <jaredalyon@gmail.com>
2021-04-30 19:29:44 -0700

    Update checkstreaks.js

--- currency/checkstreaks.js
+++ currency/checkstreaks.js
@@ -1,20 +1,38 @@
 module.exports = {
     name: "checkstreaks",
     permission: 1,
-    main: function (bot, msg) {
+    main: async function (bot, msg) {
+        /*
+        console.log("Initializing check...");
         var message = "**__Active Daily Streaks:__**\n*Does not include streaks that have expired.*\n";
+        console.log(message);
  
-        let members = bot.guilds.cache.get("399740385221672970").members.cache;
-        for (let member of members) {
-            bot.streaks.get(member.id).then(streaks => {
+        function checkStreaks() {
+            bot.guilds.cache.get("399740385221672970").members.cache.forEach(async member => {
+                let streaks = (await bot.streaks.get(member.id)) || {};
                 if (streaks.streak && streaks.streak >= 1 && (new Date() - new Date(streaks.lastDaily) <= 172800000)) {
-                    message += member.displayName + "\n" + ",";
+                    message += member.displayName + "\n";
+                    msg.channel.send(member.displayName + " has an active streak!");
                     console.log("Added " + member.displayName + "'s streak to the array!");
                 }
             });
-        };
+        }
 
-        //sends the message
+
+        console.log("Running check...");
+        await checkStreaks();
+        console.log("Check complete!");
+
+        console.log("Sending message...");
         msg.channel.send(message);
+        console.log("Message sent!");
+        */
+
+        bot.guilds.cache.get("399740385221672970").members.cache.forEach(async member => {
+            let streaks = (await bot.streaks.get(member.id)) || {};
+            if (streaks.streak && streaks.streak >= 1 && (new Date() - new Date(streaks.lastDaily) <= 172800000)) {
+                msg.channel.send(member.displayName + " has an active streak!");
+            }
+        });
     }
 }
